f6d1a8af90fe17f632081826fcaa98f15a9c1ea3
Leandro Aluisio Scholz <leandro.a.scholz@gmail.com>
2019-03-14 20:15:31 +0100

    enabled upload_data and upload_metrics

--- wrapper.py
+++ wrapper.py
@@ -25,28 +25,20 @@ def main(argv):
         
         return_code = call(command, shell=True, cwd="/app")  # waits for the subprocess to return
 
-        
-        #shArgs = ["python ", "/app/workflow.py ", in_path, out_path]
-        #return_code = call(" ".join(shArgs), shell=True, cwd="./")
-        #command = "python /workflow.py" \"\"input={},output={}\"\".format(in_path, out_path)
-        #return_code = call(command, shell=True, cwd="/")  # waits for the subprocess to return
-        #if return_code != 0:
-        #    err_desc = "Failed to execute the Vaa3D (return code: {})".format(return_code)
-        #    nj.job.update(progress=50, statusComment=err_desc)
-        #    raise ValueError(err_desc)
         # 4. Upload the annotation and labels to Cytomine 
         #    (annotations are extracted from the mask using
         # the AnnotationExporter module)
-        # upload_data(problem_cls, nj, in_images, out_path, \
-        #             **nj.flags, is_2d=is_2d, monitor_params={
-        #     "start": 60, "end": 90,
-        #     "period": 0.1,
-        #     "prefix": "Extracting and uploading polygons from masks"
-        # })
+        upload_data(problem_cls, nj, in_images, out_path, \
+                     **nj.flags, is_2d=is_2d, monitor_params={
+             "start": 60, "end": 90,
+             "period": 0.1,
+             "prefix": "Extracting and uploading polygons from masks"
+        })
         # # 5. Compute and upload the metrics
-        # nj.job.update(progress = 80, \
-        #               statusComment = "Computing and uploading metrics (if necessary)...")
-        # upload_metrics(problem_cls, nj, in_images, gt_path, out_path, tmp_path, **nj.flags)
-        # nj.job.update(status=Job.TERMINATED, progress=100, statusComment="Finished.")
+        nj.job.update(progress = 80, \
+                      statusComment = "Computing and uploading metrics (if necessary)...")
+        upload_metrics(problem_cls, nj, in_images, gt_path, out_path, tmp_path, **nj.flags)
+        nj.job.update(status=Job.TERMINATED, progress=100, statusComment="Finished.")
+        
 if __name__ == "__main__":
     main(sys.argv[1:])
