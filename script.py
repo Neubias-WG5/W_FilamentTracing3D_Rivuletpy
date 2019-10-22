1a9fbf57b0d2b38eceb237d2924324f81d8c6f80
Benjamin Pavie <benjamin.pavie@gmail.com>
2019-10-22 14:03:00 +0200

    add a fix so if the threshold params is 0 which is set to None is set back to 0 in script.py. Fixed some indentation

--- script.py
+++ script.py
@@ -14,15 +14,19 @@ parser.add_argument('--outfld', help='output folder', required=True)
 # UNCOMMENT THE NEXT TWO LINES AFTER ADDING THE WORKFLOW TO BIAFLOWS
 parser.add_argument('--threshold_value', help='threshold intensity value', required=True)
 parser.add_argument('--quality_run', \
-                   # help='outputs trace with higher quality (or not)', \
-                   # required=True)
+                    help='outputs trace with higher quality (or not)', \
+                    required=True)
 args = parser.parse_args()
 in_path = args.infld + '/'
 out_path = args.outfld + '/'
 
 print(out_path + ' is the output file path')
 # Functional parameters UNCOMMENT THE NEXT TWO LINES AFTER ADDING THE WORKFLOW TO BIAFLOWS
-threshold_value = float(args.threshold_value) # default: 0
+#Add a test to see if the threshold parameter value is None (Default value is zero but somehow 0 is changed to None)
+if args.threshold_value is not None:
+    threshold_value=0
+else:
+    threshold_value = float(args.threshold_value) # default: 0
 quality_run = bool(args.quality_run) # defalut: False 
 # temporary attribute values to test locally COMMENT THE FOLLOWING TWO LINES AFTER ADDING THE WORKFLOW TO BIAFLOWS
 # threshold_value = '5'
@@ -41,17 +45,24 @@ for neubias_input_image in in_images:
     out_file_path = out_path + neubias_input_image
 
     print('---------------------------')
-    print('Doing '+ in_path + neubias_input_image + \
-        ' and saving the output to ' + \
-        out_path)
+    #print('Doing '+ in_path + neubias_input_image + \
+    #    ' and saving the output to ' + \
+    #    out_path)
+    print("Doing {}{} and saving the output to {}".format(in_path,neubias_input_image,out_path))
 
     #Compute the neuron tracing with set parameters
     if quality_run is True:
-      command = "rtrace -f " + in_file_path + \
-      " -o " + out_file_path[:-4] + ".swc --threshold " + threshold_value + " --quality"
+        command = "rtrace -f {} -o {}.swc --threshold {} --quality".format(in_file_path,
+                                                                            out_file_path[:-4],
+                                                                            threshold_value)
+      #command = "rtrace -f " + in_file_path + \
+      #" -o " + out_file_path[:-4] + ".swc --threshold " + threshold_value + " --quality"
     else:
-      command = "rtrace -f " + in_file_path + \
-      " -o " + out_file_path[:-4] + ".swc --threshold " + threshold_value
+        command = "rtrace -f {} -o {}.swc --threshold {}".format(in_file_path,
+                                                                  out_file_path[:-4],
+                                                                  threshold_value)
+      #command = "rtrace -f " + in_file_path + \
+      #" -o " + out_file_path[:-4] + ".swc --threshold " + threshold_value
 
     print("Run tracing workflow:"+command)
     return_code = call(command, shell=True, cwd="/") # waits for the subprocess to return
--- wrapper.py
+++ wrapper.py
@@ -12,31 +12,34 @@ def main(argv):
         is_2d = False
         # 1. Create working directories on the machine
         # 2. Download the images
-        in_images, gt_images, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, nj, is_2d=is_2d, **nj.flags)
+        in_images, gt_images, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, nj, is_2d=is_2d,
+                                                                                  **nj.flags)
         # 3. Call the image analysis workflow using the run script
         nj.job.update(progress=25, statusComment="Launching workflow...")
-        
+
+        #But with the threshold value
+        #print("DEBUG TO REMOVE Threshold param value: {}".format(nj.parameters.threshold_value))
+
         # UNCOMMENT THE NEXT LINE (WITH \ ) WHEN THE WORKFLOW IS ADDED TO BIAFLOWS 
         command = "python script.py --infld {} --outfld {} \
-                   --threshold_value {} --quality_run {}".format(in_path, out_path, nj.parameters.threshold_value, nj.parameters.quality_run)
+                   --threshold_value {} --quality_run {}".format(in_path, out_path, nj.parameters.threshold_value,
+                                                                 nj.parameters.quality_run)
         # Command to test the workflow before it is added to biaflows, which does 
         #   not accept the desciptor.json parameters 
         # command = "python script.py --infld {} --outfld {}".format(in_path, out_path)
-        
+        print(command)
         return_code = call(command, shell=True, cwd="/app")  # waits for the subprocess to return
 
         # 4. Upload the annotation and labels to Cytomine 
         #    (annotations are extracted from the mask using
         # the AnnotationExporter module)
-        upload_data(problem_cls, nj, in_images, out_path, \
-                     **nj.flags, is_2d=is_2d, monitor_params={
+        upload_data(problem_cls, nj, in_images, out_path, **nj.flags, is_2d=is_2d, monitor_params={
              "start": 60, "end": 90,
              "period": 0.1,
              "prefix": "Extracting and uploading polygons from masks"
         })
         # # 5. Compute and upload the metrics
-        nj.job.update(progress = 80, \
-                      statusComment = "Computing and uploading metrics (if necessary)...")
+        nj.job.update(progress=80, statusComment="Computing and uploading metrics (if necessary)...")
         upload_metrics(problem_cls, nj, in_images, gt_path, out_path, tmp_path, **nj.flags)
         nj.job.update(status=Job.TERMINATED, progress=100, statusComment="Finished.")
         
